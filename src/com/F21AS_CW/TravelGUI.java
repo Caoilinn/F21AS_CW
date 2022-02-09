e07309510fc55c2403b05178316da75f1763dced
ztaa1 <z.t.alsultan@gmail.com>
2022-02-09 12:42:06 +0000

    Initial Setup
    Skeleton code for all relevant functions,, variables , panels, and overriding the actionPerformed method

--- src/com/F21AS_CW/TravelGUI.java
+++ src/com/F21AS_CW/TravelGUI.java
@@ -1,4 +1,35 @@
 package com.F21AS_CW;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TravelGUI {
+public class TravelGUI extends JFrame implements ActionListener
+{
+    // Instance variables
+        // Declare relevant lists
+        // Declare GUI components
+
+    // Constructor
+        // public TravelGUI()
+        // {
+        //     // Assign lists to instance variables
+        // }
+
+    // Methods to set up all relevant panels
+
+    // When a button is clicked, execute appropriate action
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        // if (e.getSource() == search)
+        // {
+        //     // Do something
+        // }
+        // else if (e.getSource() == close)
+        // {
+        //     // Do something
+        // }
+    }
 }
+
+
