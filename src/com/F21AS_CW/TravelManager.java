11db3a1aae9eac21c109bf12c3199bef0d0060e9
ztaa1 <z.t.alsultan@gmail.com>
2022-02-09 12:26:01 +0000

    Initial Setup
    Skeleton code for all major areas: list creation for all relevant classes, calling the read/write methods from within run(), method to show and initialize the GUI

--- src/com/F21AS_CW/TravelManager.java
+++ src/com/F21AS_CW/TravelManager.java
@@ -1,4 +1,38 @@
 package com.F21AS_CW;
 
-public class TravelManager {
+public class TravelManager
+{
+    // Instance variables
+        // private Airlines airlinesList;
+        // private Flights flightsList;
+        // private Airports airportsList;
+        // private Aeroplane aeroplanesList;
+
+    // Constructor
+    public TravelManager()
+    {
+        // Initialise all lists
+            // airlinesList = new Airlines();
+            // flightsList = new Flights();
+            // airportsList = new Airports();
+            // aeroplanesList = new Aeroplane();
+    }
+
+    public void run()
+    {
+        // Call read methods for all relevant classes
+            // airlinesList.readFromFile("AirlinesInput.csv");
+            // flightsList.readFromFile("FlightsInput.csv");
+            // airportsList.readFromFile("AirportsInput.csv");
+            // aeroplanesList.readFromFile("AeroplaneInput.csv");
+
+        // Call a single write method
+            // objectName.writeToFile("output.txt", objectName.exitReport());
+    }
+
+    public void showTravelGUI()
+    {
+        // Create a new TravelGUI object and pass on the created lists
+        // Set up general display parameters for the GUI
+    }
 }
