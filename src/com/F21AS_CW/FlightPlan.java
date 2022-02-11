4793f660c824d93ee088de8c7331d54704443faa
Adam Sutherland <ywb16167'uni.strath.ac.uk>
2022-02-11 18:15:26 +0000

    Added base code to FlightPlan
    FlightPlan will take a LinkedList of ControlTowers as a parameter in it's constructor, provides a method for adding Control towers to the end of said List and a getter method for the LinkedList

--- src/com/F21AS_CW/FlightPlan.java
+++ src/com/F21AS_CW/FlightPlan.java
@@ -1,4 +1,20 @@
 package com.F21AS_CW;
 
+import org.w3c.dom.Node;
+
+import java.util.LinkedList;
+
 public class FlightPlan {
+    private LinkedList<ControlTower> flightPlan;
+    public FlightPlan(LinkedList<ControlTower> fp) {
+        flightPlan = fp;
+    }
+
+    public void addToPlan(ControlTower node) {
+        flightPlan.addLast(node);
+    }
+
+    public LinkedList<ControlTower> getFlightPlan() {
+        return flightPlan;
+    }
 }
