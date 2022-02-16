91464410a9f514fbd032c39ef55342b84bbea6a3
William Oliver <williamcaoliver@gmail.com>
2022-02-16 11:51:44 +0000

    Returned Previous Work
    Aeroplane and aeroplanes added old code

--- src/com/F21AS_CW/Aeroplanes.java
+++ src/com/F21AS_CW/Aeroplanes.java
@@ -1,6 +1,18 @@
 package com.F21AS_CW;
+import java.util.HashSet;
 
 public class Aeroplanes implements IWriteable{
+
+    private HashSet<String> aeroplanes;
+
+    public Aeroplanes(HashSet<String> aeroplanes) {
+        this.aeroplanes = aeroplanes;
+    }
+
+    public HashSet<String> getAeroplanes() {
+        return aeroplanes;
+    }
+
     @Override
     public boolean WriteToFile() {
         return false;
