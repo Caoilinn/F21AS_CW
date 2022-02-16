45c5ff77ee0adf5219ad9c0ca5f4174f966bc99c
Caoilinn <32359346+Caoilinn@users.noreply.github.com>
2022-02-16 18:36:32 +0000

    Finished basic implementation of Data Classes
    All data classes now have their constructors and respective getters and setters.
    Flights class has an appropriate add method that throws exceptions when necessary.

--- src/com/F21AS_CW/Airline.java
+++ src/com/F21AS_CW/Airline.java
@@ -1,4 +1,19 @@
 package com.F21AS_CW;
 
 public class Airline {
+    private String name;
+    private String code;
+
+    public Airline(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
--- src/com/F21AS_CW/Airlines.java
+++ src/com/F21AS_CW/Airlines.java
@@ -1,6 +1,23 @@
 package com.F21AS_CW;
 
+import java.util.HashSet;
+
 public class Airlines implements IWriteable{
+
+    private HashSet<Airline> airlines;
+
+    public Airlines(HashSet<Airline> airlines) {
+        this.airlines = airlines;
+    }
+
+    public Airlines(){
+
+    }
+
+    public HashSet<Airline> getAirlines() {
+        return airlines;
+    }
+
     @Override
     public boolean WriteToFile() {
         return false;
