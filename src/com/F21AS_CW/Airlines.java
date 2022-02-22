80b9fb0841a01631e717ee685e7dedf4b29db837
Maleeha <60137714+maleehaR@users.noreply.github.com>
2022-02-22 18:10:46 +0000

    Added Code to read and store airlines, need to discuss structure for this

--- src/com/F21AS_CW/Airlines.java
+++ src/com/F21AS_CW/Airlines.java
@@ -1,20 +1,40 @@
 package com.F21AS_CW;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class Airlines implements IWriteable{
 
-    private HashSet<Airline> airlines;
+    private HashSet<String> airlines;
 
-    public Airlines(HashSet<Airline> airlines) {
-        this.airlines = airlines;
-    }
+    public Airlines(HashSet<String> airlines) {this.airlines = airlines;}
 
     public Airlines(){
+        try {
+           // HashSet<String> airlinesList = new HashSet<String>();
+            File airlinesFile = new File("Airlines");
+            Scanner Reader = new Scanner(airlinesFile);
+            while (Reader.hasNextLine()) {
+                String airline = Reader.nextLine();
+                //String line = Reader.nextLine();
+                // String [] fields = line.split(";");
+                // String airlineCode = fields[0];
+                //String airline = fields[1];
+                airlines.add(airline);
+                //System.out.println(airline);
+            }
+            System.out.println(airlines);
+            Reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
 
     }
 
-    public HashSet<Airline> getAirlines() {
+    public HashSet<String> getAirlines() {
         return airlines;
     }
 
